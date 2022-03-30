782acfaad497106a073e4e8121c86a03e584183a
Michael Woolnough <michael.woolnough@gmail.com>
2022-03-30 07:57:46 +0100

    converted memio.Buffer to bytes.Buffer for tests

--- dos2unix_test.go
+++ dos2unix_test.go
@@ -3,43 +3,41 @@ package dos2unix
 import (
 	"bytes"
 	"io"
+	"strings"
 	"testing"
-
-	"vimagination.zapto.org/memio"
 )
 
 func TestDOS2Unix(t *testing.T) {
 	buf := make([]byte, 10)
-	output := make(memio.Buffer, 0, 100)
+	output := bytes.NewBuffer(make([]byte, 0, 100))
 	for n, test := range []struct {
-		Input, Output memio.Buffer
+		Input, Output string
 	}{
 		{
-			memio.Buffer("Hello"),
-			memio.Buffer("Hello"),
+			"Hello",
+			"Hello",
 		},
 		{
-			memio.Buffer("Hello\r\n"),
-			memio.Buffer("Hello\n"),
+			"Hello\r\n",
+			"Hello\n",
 		},
 		{
-			memio.Buffer("Hello\r\nWorld"),
-			memio.Buffer("Hello\nWorld"),
+			"Hello\r\nWorld",
+			"Hello\nWorld",
 		},
 		{
-			memio.Buffer("qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n"),
-			memio.Buffer("qwertyuiop\nasdfghjkl\nzxcvbnm\n"),
+			"qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n",
+			"qwertyuiop\nasdfghjkl\nzxcvbnm\n",
 		},
 		{
-			memio.Buffer("qwertyuiop\rasdfgkl\rzxcbnm\r"),
-			memio.Buffer("qwertyuiop\rasdfgkl\rzxcbnm\r"),
+			"qwertyuiop\rasdfgkl\rzxcbnm\r",
+			"qwertyuiop\rasdfgkl\rzxcbnm\r",
 		},
 	} {
 		for i := 1; i < 10; i++ {
-			output := output
-			input := test.Input
-			io.CopyBuffer(&output, DOS2Unix(&input), buf[:i])
-			if !bytes.Equal(output, test.Output) {
+			output.Reset()
+			io.CopyBuffer(output, DOS2Unix(strings.NewReader(test.Input)), buf[:i])
+			if !bytes.Equal(output.Bytes(), []byte(test.Output)) {
 				t.Errorf("test %d.%d: expected output: %q, got %q", n+1, i, test.Output, output)
 			}
 		}
@@ -48,32 +46,31 @@ func TestDOS2Unix(t *testing.T) {
 
 func TestUnix2DOS(t *testing.T) {
 	buf := make([]byte, 10)
-	output := make(memio.Buffer, 0, 100)
+	output := bytes.NewBuffer(make([]byte, 0, 100))
 	for n, test := range []struct {
-		Input, Output memio.Buffer
+		Input, Output string
 	}{
 		{
-			memio.Buffer("Hello"),
-			memio.Buffer("Hello"),
+			"Hello",
+			"Hello",
 		},
 		{
-			memio.Buffer("Hello\n"),
-			memio.Buffer("Hello\r\n"),
+			"Hello\n",
+			"Hello\r\n",
 		},
 		{
-			memio.Buffer("Hello\nWorld"),
-			memio.Buffer("Hello\r\nWorld"),
+			"Hello\nWorld",
+			"Hello\r\nWorld",
 		},
 		{
-			memio.Buffer("qwertyuiop\nasdfghjkl\nzxcvbnm\n"),
-			memio.Buffer("qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n"),
+			"qwertyuiop\nasdfghjkl\nzxcvbnm\n",
+			"qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n",
 		},
 	} {
 		for i := 1; i < 10; i++ {
-			output := output
-			input := test.Input
-			io.CopyBuffer(&output, Unix2DOS(&input), buf[:i])
-			if !bytes.Equal(output, test.Output) {
+			output.Reset()
+			io.CopyBuffer(output, Unix2DOS(strings.NewReader(test.Input)), buf[:i])
+			if !bytes.Equal(output.Bytes(), []byte(test.Output)) {
 				t.Errorf("test %d.%d: expected output: %q, got %q", n+1, i, test.Output, output)
 			}
 		}
--- writer_test.go
+++ writer_test.go
@@ -3,9 +3,8 @@ package dos2unix
 import (
 	"bytes"
 	"io"
+	"strings"
 	"testing"
-
-	"vimagination.zapto.org/memio"
 )
 
 type jr struct {
@@ -14,40 +13,40 @@ type jr struct {
 
 func TestDOS2UnixWriter(t *testing.T) {
 	buf := make([]byte, 10)
+	output := bytes.NewBuffer(make([]byte, 0, 100))
 	for n, test := range []struct {
-		Input, Output memio.Buffer
+		Input, Output string
 	}{
 		{
-			memio.Buffer("Hello"),
-			memio.Buffer("Hello"),
+			"Hello",
+			"Hello",
 		},
 		{
-			memio.Buffer("Hello\r\n"),
-			memio.Buffer("Hello\n"),
+			"Hello\r\n",
+			"Hello\n",
 		},
 		{
-			memio.Buffer("Hello\r\nWorld"),
-			memio.Buffer("Hello\nWorld"),
+			"Hello\r\nWorld",
+			"Hello\nWorld",
 		},
 		{
-			memio.Buffer("qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n"),
-			memio.Buffer("qwertyuiop\nasdfghjkl\nzxcvbnm\n"),
+			"qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n",
+			"qwertyuiop\nasdfghjkl\nzxcvbnm\n",
 		},
 		{
-			memio.Buffer("qwertyuiop\rasdfgkl\rzxcbnm\r"),
-			memio.Buffer("qwertyuiop\rasdfgkl\rzxcbnm\r"),
+			"qwertyuiop\rasdfgkl\rzxcbnm\r",
+			"qwertyuiop\rasdfgkl\rzxcbnm\r",
 		},
 	} {
 		for i := 1; i < 10; i++ {
-			var output memio.Buffer
-			input := test.Input
-			d := DOS2UnixWriter(&output)
-			_, err := io.CopyBuffer(d, jr{&input}, buf[:i])
+			output.Reset()
+			d := DOS2UnixWriter(output)
+			_, err := io.CopyBuffer(d, strings.NewReader(test.Input), buf[:i])
 			if err != nil {
 				t.Errorf("test %d.%d: unexpected write error: %s", n+1, i, err)
 			} else if err := d.Flush(); err != nil {
 				t.Errorf("test %d.%d: unexpected flush error: %s", n+1, i, err)
-			} else if !bytes.Equal(output, test.Output) {
+			} else if !bytes.Equal(output.Bytes(), []byte(test.Output)) {
 				t.Errorf("test %d.%d: expected output: %q, got %q", n+1, i, test.Output, output)
 			}
 		}
@@ -56,33 +55,33 @@ func TestDOS2UnixWriter(t *testing.T) {
 
 func TestUnix2DOSWriter(t *testing.T) {
 	buf := make([]byte, 10)
+	output := bytes.NewBuffer(make([]byte, 0, 100))
 	for n, test := range []struct {
-		Input, Output memio.Buffer
+		Input, Output string
 	}{
 		{
-			memio.Buffer("Hello"),
-			memio.Buffer("Hello"),
+			"Hello",
+			"Hello",
 		},
 		{
-			memio.Buffer("Hello\n"),
-			memio.Buffer("Hello\r\n"),
+			"Hello\n",
+			"Hello\r\n",
 		},
 		{
-			memio.Buffer("Hello\nWorld"),
-			memio.Buffer("Hello\r\nWorld"),
+			"Hello\nWorld",
+			"Hello\r\nWorld",
 		},
 		{
-			memio.Buffer("qwertyuiop\nasdfghjkl\nzxcvbnm\n"),
-			memio.Buffer("qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n"),
+			"qwertyuiop\nasdfghjkl\nzxcvbnm\n",
+			"qwertyuiop\r\nasdfghjkl\r\nzxcvbnm\r\n",
 		},
 	} {
 		for i := 1; i < 10; i++ {
-			var output memio.Buffer
-			input := test.Input
-			_, err := io.CopyBuffer(Unix2DOSWriter(&output), jr{&input}, buf[:i])
+			output.Reset()
+			_, err := io.CopyBuffer(Unix2DOSWriter(output), strings.NewReader(test.Input), buf[:i])
 			if err != nil {
 				t.Errorf("test %d.%d: unexpected error: %s", n+1, i, err)
-			} else if !bytes.Equal(output, test.Output) {
+			} else if !bytes.Equal(output.Bytes(), []byte(test.Output)) {
 				t.Errorf("test %d.%d: expected output: %q, got %q", n+1, i, test.Output, output)
 			}
 		}
